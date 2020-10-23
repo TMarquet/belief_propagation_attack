150789a804eb71d9ee0984e1944c6be7fd2cf43d
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-10-23 16:23:25 +0200

    tests

--- belief_propagation_attack/gexf_graphCreator.py
+++ belief_propagation_attack/gexf_graphCreator.py
@@ -41,9 +41,11 @@ def check_variable_factor_edge_match(variables, factors, edges):
     error = False
     for edge in edges:
         if edge[0] not in variables:
+            print('HERE1')
             if DEBUG: print("!!! Edge {}, Variable {} not previously defined".format(edge, edge[0]))
             error = True
         elif edge[1] not in factors:
+            print('HERE2')
             if DEBUG: print("!!! Edge {}, Factor {} not previously defined".format(edge, edge[1]))
             error = True
     if error:
